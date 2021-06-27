8045fe3a214cc125fdaaefd25e25ee58c07ee412
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2021-06-27 11:07:42 +0530

    changes to add small test case

--- nest-template/src/films/films.service.spec.ts
+++ nest-template/src/films/films.service.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmsService } from './films.service';
+import { closeInMongodConnection, rootMongooseTestModule } from '../../test/util';
+import {MongooseModule} from "@nestjs/mongoose";
+import {FilmSchema} from "./entities/film.schema";
+import {CreateFilmDto} from "./dto/create-film.dto";
 
 describe('FilmsService', () => {
   let service: FilmsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: 'Film', schema: FilmSchema }]),
+      ],
       providers: [FilmsService],
     }).compile();
 
@@ -14,5 +22,18 @@ describe('FilmsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should add film', async () => {
+
+    const c = new CreateFilmDto("random","random","SHOULD_BE_DATE",1,1,"US","DRAMA","url" );
+    const savedData = await service.create(c);
+    expect(savedData).toBeDefined();
+
+    expect(savedData._id).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
   });
 });
